c320b010ba726d67bf898fe78d1f87638ee2c3c2
Mariano Matias Pereyra <marianopereyra95@gmail.com>
2020-07-20 23:24:13 -0300

    Seccion 1 - Primer componente!

---
+++ s1-introduccion/index.js
@@ -0,0 +1,23 @@
+class Hola extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hola!!!</h1>;
+                <h1>Hola!!!</h1>;
+                <h1>Hola!!!</h1>;
+            </div>
+        )
+    }
+}
+
+/* 
+function hola() {
+    return (
+        <div>
+                <h1>Hola!!!</h1>;
+                <h1>Hola!!!</h1>;
+                <h1>Hola!!!</h1>;
+        </div>
+    )
+}
+ReactDOM.render(<Hola/>, document.querySelector("#root")); */
